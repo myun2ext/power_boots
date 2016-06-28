7d73c912d9614bfbabc86e0c708cd26b2b97a5f8
myun2 <myun2@nwhite.info>
2016-06-28 16:13:53 +0900

    Tag class with attributes

--- lib/power_boots/html/tag.rb
+++ lib/power_boots/html/tag.rb
@@ -1,18 +1,27 @@
 module PowerBoots
   module Html
     class Tag
-      attr_reader :name, :content
+      attr_reader :name, :content, :attributes
 
-      def initialize(name, content = "", close: true)
+      def initialize(name, content = "", attributes: nil, close: true)
         @name = name
         @content = content
         @close = close
+        @attributes = attributes
       end
 
       def to_s
-        s = "<#{name}>" + @content
+        s = "<#{name}#{attributes_to_s}>" + @content
         s += "</#{name}>" if @close
         s
+      end
+
+      private
+      def attributes_to_s
+        return "" unless attributes
+        attributes.map { |name, value|
+          " #{name}=\"#{value}\""
+        }.join
       end
     end
   end
--- spec/power_boots/html/tag_spec.rb
+++ spec/power_boots/html/tag_spec.rb
@@ -1,16 +1,36 @@
 require 'power_boots/html/tag'
 
 describe PowerBoots::Html::Tag do
-  let(:tag) { PowerBoots::Html::Tag.new(:name, 'Content Text.', close: close) }
+  let(:tag) { PowerBoots::Html::Tag.new(:name, 'Content Text.', attributes: attributes, close: close) }
   let(:tag_text) { tag.to_s }
 
-  context "with close" do
-    let(:close) { true }
-    it { expect(tag_text).to eq "<name>Content Text.</name>" }
+  context "no attributes" do
+    let(:attributes) { nil }
+    context "with close" do
+      let(:close) { true }
+      it { expect(tag_text).to eq "<name>Content Text.</name>" }
+    end
+
+    context "none close" do
+      let(:close) { false }
+      it { expect(tag_text).to eq "<name>Content Text." }
+    end
   end
 
-  context "none close" do
-    let(:close) { false }
-    it { expect(tag_text).to eq "<name>Content Text." }
+  context "with attributes" do
+    let(:attributes) { {
+      foo: :bar,
+      hoge: :huga
+    } }
+
+    context "with close" do
+      let(:close) { true }
+      it { expect(tag_text).to eq '<name foo="bar" hoge="huga">Content Text.</name>' }
+    end
+
+    context "none close" do
+      let(:close) { false }
+      it { expect(tag_text).to eq '<name foo="bar" hoge="huga">Content Text.' }
+    end
   end
 end
