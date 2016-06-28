4362b7fcdd1b500ab2602e17e9a4ac01bb6ed1d7
myun2 <myun2@nwhite.info>
2016-06-28 19:34:50 +0900

    bind and port

--- lib/power_boots/sinatra.rb
+++ lib/power_boots/sinatra.rb
@@ -1,7 +1,12 @@
-require 'sinatra'
+require 'sinatra/base'
 
 module PowerBoots
+  class Sinatra < Sinatra::Base
+    set :bind, '0.0.0.0'
+    set :port, 4568
+  end
+
   def self.sinatra(&block)
-    PowerBoots::Sinatra.new
+    PowerBoots::Sinatra.run!
   end
 end
