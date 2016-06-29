fb8b9e7068dbe1226eeeca13ccfc223dc99701fc
myun2 <myun2@nwhite.info>
2016-06-29 14:41:13 +0900

    Sinatra bootstrap

--- lib/power_boots/sinatra.rb
+++ lib/power_boots/sinatra.rb
@@ -4,6 +4,10 @@ module PowerBoots
   class Sinatra < Sinatra::Base
     set :bind, '0.0.0.0'
     set :port, 4568
+
+    get '/' do
+      "Hello world!"
+    end
   end
 
   def self.sinatra(&block)
