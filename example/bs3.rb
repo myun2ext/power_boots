df6858225a9fc08a1a70366a9f34e76a203b57da
myun2 <myun2@nwhite.info>
2016-07-06 15:41:57 +0900

    Cp app to bs3.rb (example)

---
+++ example/bs3.rb
@@ -0,0 +1,37 @@
+require 'power_boots'
+require 'sinatra'
+
+def page(path, title: nil, &block)
+  get path do
+    PowerBoots::Document.new(:bs3) do |app|
+      app.app_name "Power Boots"
+      app.navigations(
+        'Home' => '/',
+        'About' => '/about',
+        'Contact' => '/contact'
+      )
+      app.active_page title
+      app.title title
+      app.main(&block)
+    end.render_html
+  end
+end
+
+page '/' do |con|
+  con.p "Power Bootstrap and templates.", class: :lead
+  con.h3 "Login"
+
+  con.form style: "width: 200px" do |f|
+    f.input     :email,    placeholder: "Your Email Address"
+    f.password  :password, placeholder: "Your Password"
+    f.submit    :login
+  end
+end
+
+page '/about', title: 'About' do |con|
+  con.p "About.", class: :lead
+end
+
+page '/contact', title: 'Contact' do |con|
+  con.p "Contact us.", class: :lead
+end
