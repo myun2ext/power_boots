39e6ed3f14094c33e8c8d8027ea0bbd1b51fbaed
myun2 <myun2@nwhite.info>
2016-07-24 22:19:53 +0000

    Update app.rb

--- example/app.rb
+++ example/app.rb
@@ -1,76 +1,47 @@
-require 'power_boots'
-require 'sinatra'
+require 'power_boots/sinatra'
+require 'power_boots/bs3'
 
-enable :sessions
-set :session_secret, 'powersecret'
+def page(name, &block)
+  app_name = "Power Boots"
 
-def User
-end
-
-#alias sinatra_get get
-def page(path, title: nil, &block)
-  get path do
-    PowerBoots::Document.new(:bs3) do |app|
-      app.app_name "Power Boots"
-      app.navigations(
-        'Home' => '/',
-        'About' => '/about',
-        'Contact' => '/contact'
-      )
-      app.active_page title
-      app.title title
-      app.main(&block)
-    end.render_html
-  end
-end
-
-page '/' do |con|
-  if session[:email]
-    con.p "Hello #{session[:email]}", class: :lead
-
-    con.form method: :post, path: '/session' do |f|
-      f.hidden :_method, 'delete'
-      f.submit :sign_out
+  bs3 do
+    head do
+      title app_name + ' ' + name
     end
-  else
-    con.p "Power Bootstrap and templates.", class: :lead
-    con.h3 "Login"
+    body style: "margin-top: 50px" do
+      nav do
+        brand app_name
+        links do
+          li "About",  '/about'
+          li "Contact",'/contact'
+        end
+        links class: 'pull-right' do
+          li "Sign in",'/sign_in'
+        end
+      end
 
-    con.form style: "width: 200px" do |f|
-      f.input     :email,    placeholder: "Your Email Address"
-      f.password  :password, placeholder: "Your Password"
-      f.submit    :login
-      f.text      ' or '
-      f.btn       '/registration', 'Sign up', :success
+      container do
+        h1 name
+        tag :div, &block
+      end
     end
   end
 end
 
-page '/about', title: 'About' do |con|
-  con.p "About.", class: :lead
-end
-
-page '/contact', title: 'Contact' do |con|
-  con.p "Contact us.", class: :lead
-end
-
-page '/registration', title: 'Sign up' do |con|
-  con.p "Registration your account.", class: :lead
-
-  con.form style: "width: 200px", method: :post do |f|
-    f.input     :email,    placeholder: "Your Email Address"
-    f.password  :password, placeholder: "Your Password"
-    f.password  :password_confirmation, placeholder: "Confirmation"
-    f.submit    :registration
+get '/' do
+  page 'Home' do
+    tag :p, "Hello Power Boots."
   end
 end
 
-post '/registration' do
-  session[:email] = params[:email]
-  redirect to('/')
+get '/about' do
+  page 'About' do
+    tag :p, "About page."
+  end
 end
 
-delete '/session' do
-  session[:email] = nil
-  redirect to('/')
+get '/contact' do
+  page 'Contact' do
+    tag :p, "Contact us."
+  end
 end
