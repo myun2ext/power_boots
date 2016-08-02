96d9cb230dc11b9e8b153c28f713443f67a41b66
myun2 <myun2@nwhite.info>
2016-08-02 20:16:49 +0900

    alert helper method

--- lib/power_boots/html/form_helpers.rb
+++ lib/power_boots/html/form_helpers.rb
@@ -1,6 +1,23 @@
 module PowerBoots
   module Html
     module FormHelpers
+      def alert(messages, type: :danger)
+        tag :div, class: "alert alert-#{type}", role: 'alert' do
+          if messages.is_a? Array
+            ul do
+              messages.each do |msg|
+                li msg
+              end
+            end
+          else
+            text messages
+          end
+        end
+      end
+      def notice(messages)   alert(messages, type: :success) end
+      def info(messages)     alert(messages, type: :info)    end
+      def warning(messages)  alert(messages, type: :warning) end
+
       def form path, attributes = {}, &block
         attributes[:action] = path
         attributes[:method] = :post unless attributes[:method]
