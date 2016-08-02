d0e7e5515cd1925221b9943a51bbf1548eb3d96c
myun2 <myun2@nwhite.info>
2016-08-02 20:34:10 +0900

    Form with validation message (with object)

--- lib/power_boots/html/form_helpers.rb
+++ lib/power_boots/html/form_helpers.rb
@@ -18,37 +18,54 @@ module PowerBoots
       def info(messages)     alert(messages, type: :info)    end
       def warning(messages)  alert(messages, type: :warning) end
 
-      def form path, attributes = {}, &block
-        attributes[:action] = path
-        attributes[:method] = :post unless attributes[:method]
-        tag :form, **attributes, &block
-      end
+      class Form < PowerBoots::Html::Tag
+        attr_reader :path, :object, :attributes
 
-      def input name, attributes = {}
-        attributes[:type] = :text unless attributes[:type]
-        attributes[:name] = name
-        attributes[:class] = "form-control" unless attributes[:class]
-        attributes[:id] = name
+        def initialize(path, object = nil, attributes, &block)
+          @path = path
+          @object = object
+          @attributes = attributes.dup
+          @attributes[:action] = path
+          @attributes[:method] = :post unless attributes[:method]
 
-        name = name.to_s
-        div class: "form-group" do
-          label name.capitalize, for: name
-          tag :input, attributes
+          super :form, @attributes do
+            if object && object.invalid?
+              alert(@object.errors.full_messages)
+            end
+            instance_eval &block
+          end
+        end
+
+        def input name, attributes = {}
+          attributes[:type] = :text unless attributes[:type]
+          attributes[:name] = name
+          attributes[:class] = "form-control" unless attributes[:class]
+          attributes[:id] = name
+
+          name = name.to_s
+          div class: "form-group" do
+            label name.capitalize, for: name
+            tag :input, attributes
+          end
+        end
+        def text_field name, attributes = {}
+          attr = attributes.dup
+          attr[:type] = :text
+          input name, attr
+        end
+        def password name = :password, attributes = {}
+          attr = attributes.dup
+          attr[:type] = :password
+          input name, attr
+        end
+
+        def submit name = :submit
+          button name.to_s.capitalize, class: "btn btn-primary"
         end
       end
-      def text_field name, attributes = {}
-        attr = attributes.dup
-        attr[:type] = :text
-        input name, attr
-      end
-      def password name = :password, attributes = {}
-        attr = attributes.dup
-        attr[:type] = :password
-        input name, attr
-      end
 
-      def submit name = :submit
-        button name.to_s.capitalize, class: "btn btn-primary"
+      def form path, object = nil, attributes = {}, &block
+        @content += Form.new(path, object, attributes, &block).to_s
       end
     end
   end
